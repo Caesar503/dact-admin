30a3e02e58054e2fd618c4198c6b7723459213b0
zhaokai <zhaokai@11.com>
2023-06-12 11:38:11 +0800

    update

---
+++ resources/lang/zh_CN/user.php
@@ -0,0 +1,16 @@
+<?php 
+return [
+    'labels' => [
+        'User' => '用户管理',
+        'user' => 'User',
+    ],
+    'fields' => [
+        'name' => '用户名称',
+        'mobile' => '手机号',
+        'password' => '密码',
+        'openid' => 'openid',
+        'access_token' => 'Token',
+    ],
+    'options' => [
+    ],
+];
